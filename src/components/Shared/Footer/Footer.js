08b7c7e7f4e226add7d38258169c37414f851aa5
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-21 19:46:41 +0600

    footer bottom section done

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,18 +1,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { faFaceAngry } from '@fortawesome/free-solid-svg-icons';
+import { faIdCard } from '@fortawesome/free-solid-svg-icons';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <footer className='py-5 footer-background-color'>
+      <div className='w-75 mx-auto'>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        {/* Bottom text & icom element */}
+        <div className='d-flex border-top border-secondary pt-4 justify-content-between align-items'>
+          <div>
+            <h6>Copyright © 2022 Sindabad. All Rights Reserved</h6>
+          </div>
+          <div>
+            <FontAwesomeIcon
+              className='fs-3 mx-1'
+              icon={faIdCard}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className='fs-3 mx-1'
+              icon={faIdCard}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className='fs-3 mx-1'
+              icon={faIdCard}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon
+              className='fs-3 mx-1'
+              icon={faIdCard}
+            ></FontAwesomeIcon>
+          </div>
+        </div>
       </div>
-      <div></div>
-    </div>
+    </footer>
   );
 };
 
