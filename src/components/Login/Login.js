477cbe39c018dfa385986226538aa5aadf518d83
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-24 15:02:30 +0600

    Login file updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 
+import googleIcon from '../../assets/google.svg';
 import './Login.css';
 
 const Login = () => {
@@ -37,20 +38,21 @@ const Login = () => {
     signInWithEmailAndPassword(data.email, data.password);
   };
   return (
-    <div className='loginForm-Container w-50 mx-lg-auto'>
-      <div className=''>
-        <div className=''>
-          <h2 className=''>Login</h2>
+    <div className='loginForm-Container'>
+      <div className='border-0'>
+        {/* Main Input & Text form Container */}
+        <div className='card text-center loginForm w-50'>
+          <h2 className='my-4 text-uppercase'>Login</h2>
           {/* form start */}
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className='mx-auto'>
               <label className=''>
-                <span className=''>Email</span>
+                {/* <span className='pe-2'>Email:</span> */}
               </label>
               <input
                 type='email'
-                placeholder='Your Email'
-                className=''
+                placeholder='EMAIL'
+                className='w-50 my-3 p-2 ps-4 border-0 rounded rounded-pill'
                 {...register('email', {
                   required: {
                     value: true,
@@ -73,12 +75,12 @@ const Login = () => {
             </div>
             <div className=''>
               <label className=''>
-                <span className=''>Password</span>
+                {/* <span className=' pe-2'>Password:</span> */}
               </label>
               <input
                 type='password'
-                placeholder='Password'
-                className=''
+                placeholder='PASSWORD'
+                className='w-50 mb-3 p-2 ps-4 border-0 rounded rounded-pill'
                 {...register('password', {
                   required: {
                     value: true,
@@ -101,24 +103,41 @@ const Login = () => {
             </div>
 
             {signInError}
-            <input className='' type='submit' value='Login' />
+            <input
+              className='login-btn mx-auto my-3'
+              type='submit'
+              value='LOGIN'
+            />
           </form>
           {/* Form End */}
 
           {/* New User Option Start */}
-          <p>
+          <p className='mb-0'>
             <small>
               New to Onlie Shop?{' '}
-              <Link className='text-primary' to='/signup'>
+              <Link
+                className='text-primary text-decoration-none fw-bold'
+                to='/signup'
+              >
                 Create New Account
               </Link>
             </small>
           </p>
-          <div className=''>OR</div>
+
+          {/* Options */}
+          <div className='d-flex justify-content-center align-items-center my-2'>
+            <hr className='border border-dark w-25 mx-6' />
+            <span className='mx-3'>OR</span>
+            <hr className='border border-dark w-25' />
+          </div>
 
           {/* Signinwith Google Button */}
-          <button onClick={() => signInWithGoogle()} className=''>
-            Continue with Google
+          <button
+            onClick={() => signInWithGoogle()}
+            className='google-login-btn mx-auto mb-4 text-uppercase'
+          >
+            <img src={googleIcon} alt="" className='' />
+            <span className='ps-2'>Continue with Google</span>
           </button>
         </div>
       </div>
