c0b1c91ab3c18494722c19092bc406e80683bcda
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-24 12:20:15 +0600

    Loader Final Design Completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loading from '../Shared/Loading/Loading';
 import Products from './Products';
 import Slider from './Slider';
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Slider></Slider>
+      <Loading></Loading>
       <Products></Products>
     </div>
   );
--- src/components/Shared/Loading/Loading.js
+++ src/components/Shared/Loading/Loading.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { BallTriangle } from 'react-loader-spinner';
+import './Loading.css';
 
 const Loading = () => {
   return (
-    <div className='d-flex justify-content-center'>
-      <div className='spinner-border' role='status'>
-        <span className='visually-hidden'>Loading...</span>
-      </div>
+    <div className='loading-tiangle'>
+      <BallTriangle height='100' width='100' color='grey' ariaLabel='loading' />
     </div>
   );
 };
