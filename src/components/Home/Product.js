31253769c6fcad02e1fbb166d1f90a4817da365e
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-24 16:41:20 +0600

    product card design modified

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -18,24 +18,30 @@ const Product = ({ product }) => {
   return (
     <div className='col-lg-3 col-md-4 col-6 mt-4'>
       {/* <h3>Product Name: {product.title}</h3> */}
-      <Card className='h-100'>
+      <Card className='h-100 shadow'>
+        <div>
+        </div>
         <Card.Img
-          className='p-4 product-image mx-auto'
+          className='p-4 product-image'
           variant='top'
           src={image}
         />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <h4>${price}</h4>
+          <Card.Title className='fs-6 text-center text-justify'>{title}</Card.Title>
+          <h4 className='text-center'>${price}</h4>
         </Card.Body>
-        <div className='d-flex justify-content-evenly mb-2'>
-          <button className='buy-btn fw-bold '>BUY NOW</button>
-          <button
-            className='cart-btn fw-bold'
-            onClick={() => addToShoppingCart(product)}
-          >
-            Add to Cart
-          </button>
+        <div className='mx-auto'>
+          <div>
+            <button className='buy-now-btn text-uppercase mb-3'>Buy Now</button>
+          </div>
+          <div>
+            <button
+              className='cart-btn  text-uppercase mb-3'
+              onClick={() => addToShoppingCart(product)}
+            >
+              Add to Cart
+            </button>
+          </div>
         </div>
       </Card>
     </div>
