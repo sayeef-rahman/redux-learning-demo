6fbacdb4eed1b276ad8273b0cf12ee56e72255c5
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-25 10:51:07 +0600

    footer modified

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,4 +1,11 @@
-import { FaFacebook, FaCcVisa, FaInstagram, FaTwitter, FaGooglePlus, FaYoutube } from 'react-icons/fa';
+import {
+  FaFacebook,
+  FaCcVisa,
+  FaInstagram,
+  FaTwitter,
+  FaGooglePlus,
+  FaYoutube,
+} from 'react-icons/fa';
 import { BsFacebook } from 'react-icons/bs';
 import React from 'react';
 import { faIdCard, faUser } from '@fortawesome/free-solid-svg-icons';
@@ -8,7 +15,6 @@ const Footer = () => {
   return (
     <footer className='py-5 footer-background-color w-100' id='footer'>
       <div className='mx-auto w-75'>
-
         {/* Footer Top Section */}
         <div className='d-flex justify-content-evenly align-items-center'>
           <div>
@@ -18,79 +24,90 @@ const Footer = () => {
               imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel
             </p>
             <h6>FOLLOW US ON SOCIAL:</h6>
-            <a
-              href='www.facebook.com'
-              className='me-2'
-            >
+            <a href='www.facebook.com' className='me-2'>
               <FaFacebook className='fs-2'></FaFacebook>
             </a>
-            <a
-              href='www.facebook.com'
-              className='me-2'
-            >
+            <a href='www.facebook.com' className='me-2'>
               <FaInstagram className='fs-2'></FaInstagram>
             </a>
-            <a
-              href='www.facebook.com'
-              className='me-2'
-            >
+            <a href='www.facebook.com' className='me-2'>
               <FaGooglePlus className='fs-2'></FaGooglePlus>
             </a>
-            <a
-              href='www.facebook.com'
-              className='me-2'
-            >
+            <a href='www.facebook.com' className='me-2'>
               <FaTwitter className='fs-2'></FaTwitter>
             </a>
-            <a
-              href='www.facebook.com'
-              className='me-2'
-            >
+            <a href='www.facebook.com' className='me-2'>
               <FaYoutube className='fs-2'></FaYoutube>
             </a>
-            
           </div>
           <div className=''>
             <h6>PRODUCTS</h6>
-            <div >
+            <div>
               <ul className='list-unstyled text-white text-decoration-none'>
-                <li><a href="google.com">About Us</a></li>
-                <li><a href="google.com">Secure Payment</a></li>
-                <li><a href="google.com">Price Drop</a></li>
-                <li><a href="google.com">New Products</a></li>
-                <li><a href="google.com">Best Sales</a></li>
+                <li>
+                  <a href='google.com'>About Us</a>
+                </li>
+                <li>
+                  <a href='google.com'>Secure Payment</a>
+                </li>
+                <li>
+                  <a href='google.com'>Price Drop</a>
+                </li>
+                <li>
+                  <a href='google.com'>New Products</a>
+                </li>
+                <li>
+                  <a href='google.com'>Best Sales</a>
+                </li>
               </ul>
             </div>
           </div>
           <div className=''>
-            <h6>PRODUCTS</h6>
-            <div >
+            <h6>OUR COMPANY</h6>
+            <div>
               <ul className='list-unstyled text-white text-decoration-none'>
-                <li><a href="google.com">About Us</a></li>
-                <li><a href="google.com">Secure Payment</a></li>
-                <li><a href="google.com">Price Drop</a></li>
-                <li><a href="google.com">New Products</a></li>
-                <li><a href="google.com">Best Sales</a></li>
+                <li>
+                  <a href='google.com'>Delivery</a>
+                </li>
+                <li>
+                  <a href='google.com'>Legal Notice</a>
+                </li>
+                <li>
+                  <a href='google.com'>Contact Us</a>
+                </li>
+                <li>
+                  <a href='google.com'>Sitemap</a>
+                </li>
+                <li>
+                  <a href='google.com'>Stories</a>
+                </li>
               </ul>
             </div>
           </div>
           <div className=''>
-            <h6>PRODUCTS</h6>
-            <div >
+            <h6>YOUR ACCOUNT</h6>
+            <div>
               <ul className='list-unstyled text-white text-decoration-none'>
-                <li><a href="google.com">About Us</a></li>
-                <li><a href="google.com">Secure Payment</a></li>
-                <li><a href="google.com">Price Drop</a></li>
-                <li><a href="google.com">New Products</a></li>
-                <li><a href="google.com">Best Sales</a></li>
+                <li>
+                  <a href='google.com'>Addresses</a>
+                </li>
+                <li>
+                  <a href='google.com'>Credit slips</a>
+                </li>
+                <li>
+                  <a href='google.com'>Orders</a>
+                </li>
+                <li>
+                  <a href='google.com'>Personal info</a>
+                </li>
               </ul>
             </div>
           </div>
         </div>
 
         {/* Bottom text & icom element */}
-        <hr style={{marginTop: '100px'}}/>
-        
+        <hr style={{ marginTop: '100px' }} />
+
         {/* Footer Bottom Sectio */}
         <div className='d-flex pt-4 justify-content-between align-items'>
           <div>
@@ -98,22 +115,9 @@ const Footer = () => {
           </div>
           <div>
             <FaCcVisa className='fs-2 mx-1'></FaCcVisa>
-            {/* <FontAwesomeIcon
-              className='fs-3 mx-1'
-              icon={faIdCard}
-            ></FontAwesomeIcon>
-            <FontAwesomeIcon
-              className='fs-3 mx-1'
-              icon={faIdCard}
-            ></FontAwesomeIcon>
-            <FontAwesomeIcon
-              className='fs-3 mx-1'
-              icon={faIdCard}
-            ></FontAwesomeIcon>
-            <FontAwesomeIcon
-              className='fs-3 mx-1'
-              icon={faIdCard}
-            ></FontAwesomeIcon> */}
+            <FaCcVisa className='fs-2 mx-1'></FaCcVisa>
+            <FaCcVisa className='fs-2 mx-1'></FaCcVisa>
+            <FaCcVisa className='fs-2 mx-1'></FaCcVisa>
           </div>
         </div>
       </div>
