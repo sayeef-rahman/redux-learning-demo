18586bedccf4b64d874e64b427be506cfda171f5
sayeef-rahman <sayeef.cse@gmail.com>
2022-07-27 12:20:54 +0600

    shoecase finalized perfectly

--- src/components/Home/Showcase.js
+++ src/components/Home/Showcase.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './Showcase.css';
-import showcase1 from '../../assets/showcase/showcase1.jpg'
-import showcase2 from '../../assets/showcase/showcase2.jpg'
-import showcase3 from '../../assets/showcase/showcase3.jpg'
+import showcase1 from '../../assets/showcase/showcase1.jpg';
+import showcase2 from '../../assets/showcase/showcase2.jpg';
+import showcase3 from '../../assets/showcase/showcase3.jpg';
 
 const Showcase = () => {
   return (
-    <div className='d-flex justify-content-evenly align-items-center showcase-container my-5'>
-      <img src={showcase1} alt="" className='w-25' />
-      <img src={showcase2} alt="" className='w-25'/>
-      <img src={showcase3} alt="" className='w-25'/>
+    <div className='mx-lg-3'>
+      <div className='row'>
+        <div className='col-lg-4 col-sm-6'>
+          <div className='single-showcase mb-5 overflow-hidden'>
+            <img src={showcase1} alt='' className='img-hover-zoom' />
+          </div>
+        </div>
+        <div className='col-lg-4 col-sm-6'>
+          <div className='single-showcase mb-5 overflow-hidden'>
+            <img src={showcase2} alt='' className='img-hover-zoom' />
+          </div>
+        </div>
+        <div className='col-lg-4 col-sm-6'>
+          <div className='single-showcase mb-5 overflow-hidden'>
+            <img src={showcase3} alt='' className='img-hover-zoom' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
