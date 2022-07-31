a1ea31ec550aef3584cb0cc5cefb48f9f4071e41
unknown <sayeef.cse@gmail.com>
2022-07-31 13:41:37 +0600

    career benefits icon setting up

--- src/components/Career/Benefits.js
+++ src/components/Career/Benefits.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './Benefits.css';
+import React from "react";
+import "./Benefits.css";
+import { GiLoveMystery } from "react-icons/gi";
+import { GrDocumentTime } from "react-icons/gr";
 
 const Benefits = () => {
-    return (
+  return (
+    <div className="container text-center">
+      <h1 className="text-uppercase text-warning fw-bold mt-5">
+        Other Benefits
+      </h1>
+      <div className="my-3">
         <div>
-            
+          <GiLoveMystery className="benefits-icons "></GiLoveMystery>
         </div>
-    );
+        <h5 className="fs-6">Comprehensive Health Plans</h5>
+      </div>
+
+      <div  className="my-3">
+        <div>
+          <GrDocumentTime className="benefits-icons"></GrDocumentTime>
+        </div>
+        <h5 className="fs-6">Paid Volunteer Time</h5>
+      </div>
+
+      <div className="my-3">
+        <div>
+          <GiLoveMystery className="benefits-icons"></GiLoveMystery>
+        </div>
+        <h5 className="fs-6">Healthy Food & Snacks</h5>
+      </div>
+
+      <div className="my-3">
+        <div>
+          <GiLoveMystery className="benefits-icons"></GiLoveMystery>
+        </div>
+        <h5 className="fs-6">Generous Parental & Family Leave</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Benefits;
