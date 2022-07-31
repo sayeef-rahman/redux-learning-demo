448466888ea691b5be6165098254d59318188334
unknown <sayeef.cse@gmail.com>
2022-07-31 15:43:37 +0600

    career page Our values done

--- src/components/Career/OurValues.js
+++ src/components/Career/OurValues.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './OurValues.css';
 import values from '../../assets/career/values.jpg';
 
 const OurValues = () => {
     return (
         <div className='container text-center mt-5 '>
-            <h1>Our values</h1>
-            <div className='w-100'>
+            <h1 className='fs-4 text-uppercase mb-3'>Our values</h1>
+            <div className='our-values-image'>
                 <img src={values} alt="" />
             </div>
         </div>
