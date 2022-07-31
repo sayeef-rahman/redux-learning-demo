42f26bb98aab0d3d827d8da97c278aac7dbe35a4
unknown <sayeef.cse@gmail.com>
2022-07-31 13:21:01 +0600

    career benefits started

---
+++ src/components/Career/Benefits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Benefits.css';
+
+const Benefits = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Benefits;
--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Benefits from './Benefits';
 import CareerTopSection from './CareerTopSection';
 import CurrentJobs from './CurrentJobs';
 
@@ -7,6 +8,7 @@ const Career = () => {
         <div>
             <CareerTopSection></CareerTopSection>
             <CurrentJobs></CurrentJobs>
+            <Benefits></Benefits>
         </div>
     );
 };
