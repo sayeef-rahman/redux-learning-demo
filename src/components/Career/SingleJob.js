5a5bd62e5c9018631c17e614ff556b8dcefceabe
unknown <ruhulamin@parallaxlogic.net>
2022-07-31 13:04:25 +0600

    Career job section done

--- src/components/Career/SingleJob.js
+++ src/components/Career/SingleJob.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
+import './SingleJob.css';
 import { HiOutlineBriefcase } from "react-icons/hi";
 import { GrMapLocation } from "react-icons/gr";
 import { FaUsersCog } from "react-icons/fa";
 import { GiTakeMyMoney } from "react-icons/gi";
 
 const SingleJob = () => {
-    return (
-        <div className="d-flex justify-content-around align-items-center border border-3 mx-auto container my-4">
-        <div className="d-flex align-items-center justify-content-center py-3 single-feature">
-          <div>
-            <HiOutlineBriefcase className="fs-1"></HiOutlineBriefcase>
-          </div>
+  return (
+    <div className="px-3 px-lg-0">
+      <div className="container d-lg-flex justify-content-lg-evenly align-items-lg-center single-job-container mx-lg-auto my-4 px-3">
+
+        <div className="d-flex pt-3 pt-lg-0 align-items-center my-auto py-lg-auto">
+          <HiOutlineBriefcase className="fs-1"></HiOutlineBriefcase>
           <div className="ms-3">
             <p className="m-0 p-0 fw-bold">Position</p>
             <p className="m-0 p-0">Front End Developer</p>
           </div>
         </div>
 
-        <div className="d-flex align-items-center justify-content-center py-3 single-feature">
+        <div className="d-flex pt-3 pt-lg-0 align-items-center my-auto py-lg-auto">
           <div>
             <GrMapLocation className="fs-1"></GrMapLocation>
           </div>
@@ -27,7 +28,7 @@ const SingleJob = () => {
           </div>
         </div>
 
-        <div className="d-flex align-items-center justify-content-center py-3 single-feature">
+        <div className="d-flex pt-3 pt-lg-0 align-items-center my-auto py-lg-auto">
           <div>
             <FaUsersCog className="fs-1"></FaUsersCog>
           </div>
@@ -37,7 +38,7 @@ const SingleJob = () => {
           </div>
         </div>
 
-        <div className="d-flex align-items-center justify-content-center py-3 single-feature">
+        <div className="d-flex pt-3 pt-lg-0 align-items-center my-auto py-lg-auto">
           <div>
             <GiTakeMyMoney className="fs-1"></GiTakeMyMoney>
           </div>
@@ -46,11 +47,12 @@ const SingleJob = () => {
             <p className="m-0 p-0">Negotiable</p>
           </div>
         </div>
-        <div>
+        <div className="py-3 text-center">
           <button className="text-dark p-2 rounded">View Details</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SingleJob;
